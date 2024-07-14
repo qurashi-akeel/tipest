1346e3b557d57094a9dc20c57e0967663a83c006
Qurashi Akeel <qurashiakeel786@gmail.com>
2024-07-14 11:46:59 +0530

    Add basic typing component

--- src/lib/pages/typing/index.tsx
+++ src/lib/pages/typing/index.tsx
@@ -1,8 +1,26 @@
+import { Box, Divider, Text, Textarea } from '@chakra-ui/react';
+
 const TypingPage = () => {
   return (
-    <div>
-      <p>TypingPage</p>
-    </div>
+    <Box>
+      <Text
+        fontSize="2xl"
+        fontWeight="bold"
+        display="flex"
+        justifyContent="space-between"
+      >
+        <span>Typing Practice</span>
+        <span>[00:00]</span>
+      </Text>
+
+      <Divider my="5" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea omnis
+        eligendi, eum assumenda inventore sint aliquid odio, saepe quo ex error
+        fuga est velit modi voluptatum facere, corporis vitae rerum.
+      </p>
+      <Textarea mt={6} placeholder="Start typing to start timer..." />
+    </Box>
   );
 };
 
